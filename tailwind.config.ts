a46ffd21ab09e969e048088c4826ced6bc8af278
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-01-14 16:49:43 +0530

    update next ui packages and imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(navbar|button|link|card).js",
+    "./node_modules/@nextui-org/theme/dist/components/(button|card|image|navbar).js",
   ],
   // safelist: ["sm:hidden ssm:flex"],
   darkMode: "class",

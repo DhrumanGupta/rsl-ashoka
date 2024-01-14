b0fa8370f78070d1d311eff6c288c7e8abd55ec9
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-01-14 18:07:10 +0530

    Update page.tsx

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Schedule() {
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="h-hero w-full flex items-center justify-center">
       <h2>Coming Soon!</h2>
     </div>
   );

61ffebef856c980144eb83029501b6566265b36b
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-01-31 18:09:26 +0530

    add firebase!

---
+++ src/hooks/useRealtimeData.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { database } from "@/firebase/config";
+import { off, onValue, ref } from "firebase/database";
+
+function useRealtimeData<T>(path: string) {
+  const [data, setData] = useState<T | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dbRef = ref(database, path);
+
+        onValue(dbRef, (snapshot) => {
+          setData(snapshot.val());
+        });
+
+        return () => off(dbRef);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, [path]);
+
+  return data;
+}
+
+export default useRealtimeData;
